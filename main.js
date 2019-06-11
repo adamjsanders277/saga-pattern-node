8aa236844de1b17176d64374c1acc3d016233525
Adam Sanders <ajsanders277@gmail.com>
2019-06-11 20:26:43 +0100

    running code

--- main.js
+++ main.js
@@ -4,23 +4,20 @@ const Promise = require('bluebird')
 // Test code ///
 let counter = 0;
 let interval;
-const numberOfOperations = 25;
+const numberOfOperations = 2;
 const listOfArguments = [];
 const listOfDelays = [];
 
 for (let i = 0; i < numberOfOperations; i++) {
   listOfArguments.push(i);
-  listOfDelays.push(Math.ceil(Math.random() * 9) * 1000);
 }
 
-const asyncOperation = index => {
-  counter++;
-  return new Promise(resolve =>
-    setTimeout(() => {
-      console.log('Operation performed:', index);
-      counter--;
-      resolve(index);
-    }, listOfDelays[index]))
+const asyncInvoke = (name, testId) => {
+  return new Promise((resolve, reject) => {
+    //const result = await dynamoDbLib.call("get", params);
+    resolve(testCall(name, testId));
+    setTimeout(() => reject(new Error("Whoops!")), 10000);
+  })
 };
 
 /////
@@ -29,56 +26,90 @@ async function runConcurrentLambda() {
   const concurrencyLimit = 5;
   const depth = 5; // TO DO - Developer - Fix depth in event of infinite recursion
 
-  const argsCopy = [].concat(listOfArguments.map((val, ind) => ({ val, ind })));
-  const results = {};
+  const argsCopy = ["1", "2"]
+  const results = {statuses: {}};
   const promises = new Array(concurrencyLimit).fill(Promise.resolve());
+
+  const params = { TableName: "house" }
 
   function nextLambda(p) {
     if (argsCopy.length) {
       const arg = argsCopy.shift();
       return p.then(() => {
         try {
-          const operationPromise = asyncOperation(arg.val).then(r => { 
-
-            const result = await dynamoDbLib.call("get", params);
-            results.body = addResultToResultObject(result.Item, results.body)
+          const operationPromise = asyncInvoke("", arg).then(result => { 
+            results.body = addResultToResultObject(params.TableName, result.Item, arg, results.body)
             if(result.Item) {
               results["statuses"][params.TableName] = success(result.Item);
-
-              add result to full json
-
-              // example 
-              // results.body = { servingLocationUUID : 1 }
-              // if success get back result.Item = { id : 1, name: "hello" }
-                  // return results.body = { servingLocation: { id: 1, name: "hello"}}
-              // else return results.body
-
-              results["body"] = 
               return nextLambda(operationPromise);
             } else {
-              result["statuses"][params.TableName] = failure({ status: false, error: "Item not found." });
+              results["statuses"][params.TableName] = failure({ status: false, error: "Item not found." });
             }
-          
+          }).catch(error => {
+            results["statuses"][params.TableName] = failure({ status: false, error: String(error)  })
           });
         } catch(e) {
-          result["statuses"][params.TableName] = failure({ status: false, error: str(e)  })
+          results["statuses"][params.TableName] = failure({ status: false, error: String(e) })
         }
       });
     }
-    return p;
   }
 
-  await Promise.all(promises.map(chainNext));
-  console.log(result)
-  return result;
+  await Promise.all(promises.map(nextLambda));
+  console.log(results)
+  return results;
 }
 
-function addResultToResultObject(result, resultObject) {
-  // example 
-              // results.body = { servingLocationUUID : 1 }
-              // if success get back result.Item = { id : 1, name: "hello" }
-                  // return results.body = { servingLocation: { id: 1, name: "hello"}}
-              // else return results.body
-  if()
-  
+function addResultToResultObject(name, result, idName, resultObject) {
+  if(!resultObject) {
+    return result
+  }
+  if(result) {
+    resultObject[name] = result
+  } else {
+    result = { "id": resultObject[idName] }
+  }
+  delete resultObject[idName]
+  return resultObject
 }
+
+function testCall(type, params) {
+  if(params == "1") {
+    return ({
+      Item : {
+        house: "hello",
+        stevenId: "1"
+      }
+    })
+  }
+  else if(params == "2") {
+    return ({
+      Item : {
+        house: "hello",
+        stevenId: "1"
+      }
+    })
+  }
+}
+
+function success(body) {
+  return buildResponse(200, body);
+}
+
+function failure(body) {
+  return buildResponse(500, body);
+}
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+      ,
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(body)
+  };
+}
+
+runConcurrentLambda()
